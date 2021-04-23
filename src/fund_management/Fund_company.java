af12317f2df8e5f5793259d1a946f06e3f3079b9
Romane-Fonseka <romanesfonseka@gmail.com>
2021-04-23 10:57:35 +0530

    commit one

---
+++ src/fund_management/Fund_company.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package fund_management;
+
+/**
+ * @author Asus
+ *
+ */
+public class Fund_company {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("PAF project fund mgt service");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Asus
+ *
+ */
+module fund_management {
+}
